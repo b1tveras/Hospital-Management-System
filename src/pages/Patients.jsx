144771f592f787f45f3aac5ced171ffe10417c05
amamsingh <mrsingheo01@gamil.com>
2026-03-11 23:28:21 +0530

    l

--- src/pages/Patients.jsx
+++ src/pages/Patients.jsx
@@ -85,7 +85,6 @@ const Patients = () => {
         )}
       </div>
 
-      </div>
       
       {error && (
         <div className="bg-red-50 dark:bg-red-900/30 text-red-600 dark:text-red-400 p-4 rounded-xl text-sm border border-red-200 dark:border-red-800/50">
