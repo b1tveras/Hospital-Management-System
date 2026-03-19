153ae95206c34e70b1f869f53ef65eb4af22a294
amamsingh <mrsingheo01@gamil.com>
2026-03-19 09:20:50 +0530

    as

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -108,8 +108,6 @@ const Doctors = () => {
           </select>
         </div>
       </div>
-
-      </div>
       
       {error && (
         <div className="bg-red-50 dark:bg-red-900/30 text-red-600 dark:text-red-400 p-4 rounded-xl text-sm border border-red-200 dark:border-red-800/50">
